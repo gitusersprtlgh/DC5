590db2a2336c849196d12e635486eae57ed77d32
Damon Carlstrom <damoncarlstrom@Damons-MacBook-Pro.local>
2013-11-26 18:21:23 -0600

    Still stuck

--- animation.php
+++ animation.php
@@ -1,7 +1,7 @@
 <div id="animation">
     
     <!-- modal-ani-2 -->
-    <div id="jungle" class="skroll-bg" data-3000="background-position: 50% 300px" data-5900="background-position: 50% 0px;"></div>
+    <div id="jungle" class="skroll-bg" data-3000="background-position: 500px 300px" data-5900="background-position: 500px 0px;"></div>
     <div id="branch"><img style="position: absolute;"src="images/branch-wood.png" alt="">
       <div id="osmplayer"></div>
     </div>
