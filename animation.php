d22f6b566ca851623c48d9c277c9c38cad59e9d0
Damon Carlstrom <damoncarlstrom@Damons-MacBook-Pro.local>
2013-12-17 18:35:05 -0600

    Not much progress

--- animation.php
+++ animation.php
@@ -1,8 +1,10 @@
 <div id="animation">
     
     <!-- modal-ani-2 -->
-    <div id="jungle" class="skroll-bg" data-3000="background-position: 500px 300px" data-5900="background-position: 500px 0px;"></div>
-    <div id="branch"><img style="position: absolute;"src="images/branch-wood.png" alt="">
+    <div id="jungle" class="skroll-bg" data-3000="top: 50px;" data-5900="top: 100px;">
+      <img src="images/jungle-back.jpg" alt="">
+    </div>
+    <div id="branch" ><img src="images/branch-wood.png" alt="">
       <div id="osmplayer"></div>
     </div>
     <div id="animation-box">
